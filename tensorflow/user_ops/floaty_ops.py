87a41132a17d7253e8e84f9262369e415de87c0b
Jon Gauthier <jon@gauthiers.net>
2016-07-12 11:01:05 -0700

    get floaty_gather_op_test passing

--- tensorflow/user_ops/floaty_ops.py
+++ tensorflow/user_ops/floaty_ops.py
@@ -11,6 +11,8 @@ from tensorflow.python.framework import common_shapes
 from tensorflow.python.framework import constant_op
 from tensorflow.python.framework import dtypes
 from tensorflow.python.framework import ops
+from tensorflow.python.ops import array_ops
+from tensorflow.python.ops import math_ops
 
 
 _module = tf.load_op_library(os.path.join(tf.resource_loader.get_data_files_path(), "floaty_ops_impl.so"))
@@ -26,4 +28,19 @@ def _FloatyGatherShape(op):
   indices_shape = op.inputs[1].get_shape()
   return [indices_shape.concatenate(params_shape[1:])]
 
+
+@ops.RegisterGradient("FloatyGather")
+def _FloatyGatherGrad(op, grad):
+  if op.inputs[0].get_shape().is_fully_defined():
+    dense_shape = constant_op.constant(op.inputs[0].get_shape().as_list())
+    values_shape = [-1] + op.inputs[0].get_shape()[1:].as_list()
+  else:
+    # op.inputs[0] can be large, so colocate the shape calculation with it.
+    with ops.colocate_with(op.inputs[0]):
+      dense_shape = array_ops.shape(op.inputs[0])
+      values_shape = array_ops.concat(0, [[-1], dense_shape[1:]])
+
+  values = array_ops.reshape(grad, values_shape)
+  indices = math_ops.to_int32(array_ops.reshape(op.inputs[1], [-1]))
+  return [ops.IndexedSlices(values, indices, dense_shape), None]
 
